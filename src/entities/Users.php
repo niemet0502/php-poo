d65a16ce29228c9f49ee7c544be40db8828a633c
niemet0502 <vincentmarius8@gmail.com>
2021-02-19 15:23:46 +0100

    main :: added User class content

---
+++ src/entities/Users.php
@@ -0,0 +1,44 @@
+<?php
+class Users{
+  private $id;
+  private $nom;
+  private $prenom;
+  private $email;
+  private $password;
+  private $etat;
+
+  //setters
+  function setId($id){
+    $this->id = $id;
+  }
+  function setNom($nom){
+    $this->nom = $nom;
+  }
+  function setPrenom($prenom){
+    $this->prenom = $prenom;
+  }
+  function setPassword($password){
+    $this->password = $password;
+  }
+  function setEtat($etat){
+    $this->etat = $etat;
+  }
+
+  //getters
+
+  function getId(){
+    return $this->id;
+  }
+  function getNom(){
+    return $this->nom;
+  }
+  function getPrenom(){
+    return $this->prenom;
+  }
+  function getPassword(){
+    return $this->password;
+  }
+  function getEtat(){
+    return $this->etat;
+  }
+}
